2e275bfb638cd283c57ec2424a601102a4611490
Haoyi Wei <113647726+wei-haoyi@users.noreply.github.com>
2022-12-14 00:39:13 -0500

    Add files via upload

---
+++ 4_exploratory data analysis/plot2.R
@@ -0,0 +1,19 @@
+# This file plot plot1.
+library(dplyr)
+## Import the data
+data0 <- read.table("~/Documents/GitHub/Coursera_Data_Science_Certificate/4_exploratory data analysis/household_power_consumption.txt", header=TRUE, sep=";", na.strings = "?", colClasses = c('character','character','numeric','numeric','numeric','numeric','numeric','numeric','numeric'))
+
+# clean the data
+data <- data0 %>%
+          mutate(Date=as.Date(Date, "%d/%m/%Y")) %>% ## Format date to Type Date
+          filter(Date>="2007-02-01" & Date<="2007-02-02") %>% ## Filter data set from Feb. 1, 2007 to Feb. 2, 2007
+          na.omit() %>% ## Remove incomplete observation
+          mutate(datetime=as.POSIXct(paste(Date,Time),format="%Y-%m-%d %H:%M:%S")) %>% #add time date column
+          select(-c(Date,Time)) %>%
+          relocate(datetime)
+
+## plot 2
+## Create Plot 2
+plot(data$Global_active_power~data$datetime, type="l", ylab="Global Active Power (kilowatts)", xlab="")
+dev.copy(png,"plot2.png", width=480, height=480)
+dev.off()
