2c127840747549590c9cbd884b1bc7cacf8702f5
Haoyi Wei <113647726+wei-haoyi@users.noreply.github.com>
2023-01-03 15:15:05 -0500

    Update prediction.R

--- 10_Data Science Capstone/shiny/prediction.R
+++ 10_Data Science Capstone/shiny/prediction.R
@@ -1,7 +1,7 @@
 # prediction.R ####
 # Coursera Data Science Capstone Project (https://www.coursera.org/course/dsscapstone)
 # Script for predicting a NextWord given an input of any length using stupid backoff algorithm
-# 2016-01-23
+# 2023-01-03
 
 # Libraries and options ####
 library(dplyr)
