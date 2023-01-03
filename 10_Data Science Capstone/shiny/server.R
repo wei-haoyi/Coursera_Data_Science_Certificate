107c64d90d8ed3a0a9e02334c2f6357c97b1367b
Haoyi Wei <113647726+wei-haoyi@users.noreply.github.com>
2023-01-03 15:13:57 -0500

    Update server.R

--- 10_Data Science Capstone/shiny/server.R
+++ 10_Data Science Capstone/shiny/server.R
@@ -1,7 +1,7 @@
 # server.R ####
 # Coursera Data Science Capstone Project (https://www.coursera.org/course/dsscapstone)
 # Shiny server script
-# 2016-01-23
+# 2023-01-03
 
 # Libraries and options ####
 source('prediction.R')
